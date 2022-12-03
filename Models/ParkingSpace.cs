ba809fb412c678caa7aff783c52a1c877bf5c503
Aoife Carroll-Breen <seaoife@yahoo.com>
2022-12-03 19:13:00 +0000

    init

---
+++ Models/ParkingSpace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace easyPark.Models
+{
+    public class ParkingSpace
+    {
+        public string Id { get; set; }
+        public string StreetName { get; set; }
+        public int TotalSpace { get; set; }
+        public string Venue { get; set; }
+        public float lat { get; set; }
+        public float lng { get; set; }
+        public bool IsGarageParking { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
