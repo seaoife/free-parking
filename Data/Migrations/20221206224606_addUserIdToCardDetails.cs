a08aacf70ed28a0a0a2b58265b0b47841a926c4e
Aoife Carroll-Breen <seaoife@yahoo.com>
2022-12-07 12:20:44 +0000

    Added back arrows and linked pages from without login to search page.

---
+++ Data/Migrations/20221206224606_addUserIdToCardDetails.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreePark.Data.Migrations
+{
+    public partial class addUserIdToCardDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "CarDetails",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "CarDetails");
+        }
+    }
+}
--- Models/CarDetails.cs
+++ Models/CarDetails.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace FreePark.Models
 {
     public class CarDetails
     {
         public int Id { get; set; }
+       
+       // [ForeignKey("UserId")]
+       //public ApplicationUser UserId { get; set; }
     }
 }
