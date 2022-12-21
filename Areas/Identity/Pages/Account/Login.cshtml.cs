2e548ffc1a5043dd86fce529d47ffe9ec5002a9f
Aoife Carroll-Breen <seaoife@yahoo.com>
2022-12-21 15:39:42 +0000

    add redirection after login, add btn to map

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -83,7 +83,7 @@ namespace FreePark.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToAction("Search", "ParkingSpaces", new { area = "" });
                 }
                 if (result.RequiresTwoFactor)
                 {
