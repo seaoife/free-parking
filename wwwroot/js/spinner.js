ff4d1f6582b86c5f9cfc07ecd5f516736d47b9e1
Aoife Carroll-Breen <seaoife@yahoo.com>
2023-01-04 19:04:18 +0000

    add car logo spinner

---
+++ wwwroot/js/spinner.js
@@ -0,0 +1,8 @@
+﻿window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+
+    })
+                })
